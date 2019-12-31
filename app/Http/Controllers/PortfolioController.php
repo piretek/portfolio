a397a20f1934ab1c002f98422a86011c904e37ec
Piotr Czarnecki <piretek@piretek.pro>
2019-12-31 14:37:02 +0100

    Telescope installation

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Storage;
 class PortfolioController extends Controller
 {
 
+    public function __construct() {
+
+        $this->middleware('auth');
+    }
+
     public function websites() {
 
         $websites = Website::orderBy('created_at', 'DESC')->get();
